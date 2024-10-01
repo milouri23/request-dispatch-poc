8bd1d109a1a0abceaaeeec1fa277c495f883a83a
Camilo Uribe <camilou@outlook.com>
2024-10-01 13:50:57 -0500

    Agregar inyección de dependencias a ejemplo básico

--- Src/Initialization/ConsoleApp.RequestDispatcher/Program.cs
+++ Src/Initialization/ConsoleApp.RequestDispatcher/Program.cs
@@ -1,11 +1,76 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace ConsoleApp.RequestDispatcher;
 
-public static class Program
+public class Program
 {
+    private readonly IBusinessLogic _businessLogic;
+
+    public Program(IBusinessLogic businessLogic)
+    {
+        _businessLogic = businessLogic;
+    }
+
+    public void Run()
+    {
+        ConsoleKey key;
+        do
+        {
+            // Business Logic
+            _businessLogic.Execute();
+
+            // Application Logic
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to continue, or any other key to exit.");
+            key = Console.ReadKey(true).Key;
+            Console.WriteLine();
+        } while (key == ConsoleKey.Enter);
+    }
+
     public static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        // Setup Dependency Injection
+        var serviceProvider = new ServiceCollection()
+            .AddSingleton<IRandomDigitService, RandomDigitService>()
+            .AddSingleton<IBusinessLogic, BusinessLogic>()
+            .AddSingleton<Program>()
+            .BuildServiceProvider();
+
+        // Resolve and Run Program
+        serviceProvider.GetRequiredService<Program>().Run();
     }
+}
+
+public interface IBusinessLogic
+{
+    void Execute();
+}
+
+public class BusinessLogic : IBusinessLogic
+{
+    private readonly IRandomDigitService _randomDigitService;
+
+    public BusinessLogic(IRandomDigitService randomDigitService)
+    {
+        _randomDigitService = randomDigitService;
+    }
+
+    public void Execute()
+    {
+        int randomDigit = _randomDigitService.Generate();
+        Console.WriteLine($"Hello Digit: {randomDigit}");
+    }
+}
+
+public interface IRandomDigitService
+{
+    int Generate();
+}
+
+public class RandomDigitService : IRandomDigitService
+{
+    private readonly Random _rnd = new();
+
+    public int Generate() => _rnd.Next(0, 10);
 }
